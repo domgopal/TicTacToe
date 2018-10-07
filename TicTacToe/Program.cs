ba8bea52cadf74fc5759803af9cb849f9acde9ff
Dominic Gopal <domgo@finsoftconsulting.com.my>
2018-10-07 20:52:41 +0800

    Evolution1 Testing

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -19,6 +19,9 @@ namespace TicTacToe
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .CaptureStartupErrors(true)
+                .UseStartup<Startup>()
+                .PreferHostingUrls(true)
+                .UseUrls("http://localhost:5000");
     }
 }
